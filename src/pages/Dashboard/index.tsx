244e96bcfd80c4116b8dbeac39a7edce8e55e84f
Eduardo Abe <eduabe.n@gmail.com>
2021-03-13 17:02:48 -0300

    Finalizado

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      api.get('/foods').then(response => {
+        setFoods(response.data);
+      })
     }
 
     loadFoods();
@@ -37,20 +39,48 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const foodInsert = {
+        ...food,
+        available: true,
+        id: foods.length + 1,
+      }
+      await api.post('/foods', foodInsert);
+      setFoods([...foods, foodInsert]);
+      toggleModal();
     } catch (err) {
       console.log(err);
     }
   }
 
   async function handleUpdateFood(
-    food: Omit<IFoodPlate, 'id' | 'available'>,
+    {name, image, price, description}: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const updatedFood = {
+      ...editingFood,
+      name,
+      image,
+      price,
+      description
+    };
+
+    await api.put(`/foods/${updatedFood.id}`, updatedFood);
+
+    const updatedFoodIndex = foods.findIndex(food => food.id === updatedFood.id);
+
+    const updatedFoods = Array.from(foods);
+
+
+    updatedFoods[updatedFoodIndex] = updatedFood;
+    setFoods(updatedFoods);
+    
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    const updatedFoods = foods.filter(food => food.id !== id);
+
+    setFoods(updatedFoods);
   }
 
   function toggleModal(): void {
@@ -62,7 +92,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
